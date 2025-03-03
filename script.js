c37298ebc5db58118064b4d97f5dbde818209983
BiniElla <mrdea22@src.pshs.edu.ph>
2025-03-03 19:08:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function CallaFunction() {
 	let Message = prompt('Enter the message:');
 	
 	if (Message.includes("FR")|| Message.includes("FR") && Message.includes("AI")) {
-		document.getElementById('result').innerHTML = 'The message "${Message}" is legitimate.';		
+		document.getElementById('result').innerHTML = `The message "${Message}" is legitimate.`;		
 	}
 	else if (Message.includes("AI") || Message.includes("aI") || Message.includes("ai")) {
-		document.getElementById('result').innerHTML = 'The message "${Message}" is tampered with.';
+		document.getElementById('result').innerHTML = `The message "${Message}" is tampered with.`;
 	}
 	else {
-		document.getElementById('result').innerHTML = 'The message "${Message}" is yet to be encoded.';
+		document.getElementById('result').innerHTML = `The message "${Message}" is yet to be encoded.`;
 	}
 }
