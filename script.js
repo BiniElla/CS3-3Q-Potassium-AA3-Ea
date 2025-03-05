9dc4d8022652278aa19173271bab5a6de49a47ce
BiniElla <mrdea22@src.pshs.edu.ph>
2025-03-05 14:17:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function conditionalFunc() {
 	let Message = prompt('Enter the message:');
 	
-	if (Message.includes("FR")|| Message.includes("FR") && Message.includes("AI")) {
-		document.getElementById('result').innerHTML = `The message "${Message}" is legitimate!`;		
+	if (Message.includes("AI") || Message.includes("aI") || Message.includes("Ai")) {
+		document.getElementById('result').innerHTML = `The message "${Message}" is tampered with!`;		
 	}
-	else if (Message.includes("AI") || Message.includes("aI") || Message.includes("Ai")) {
-		document.getElementById('result').innerHTML = `The message "${Message}" is tampered with!`;
+	else if (Message.includes("FR")|| Message.includes("FR") && Message.includes("AI")) {
+		document.getElementById('result').innerHTML = `The message "${Message}" is legitimate!`;
 	}
 	else {
 		document.getElementById('result').innerHTML = `The message "${Message}" is yet to be encoded!`;
