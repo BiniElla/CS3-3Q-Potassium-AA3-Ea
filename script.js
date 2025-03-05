a04b523b42acae893b3104fbff2321a3754ab61d
BiniElla <mrdea22@src.pshs.edu.ph>
2025-03-05 14:13:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function conditionalFunc() {
 	if (Message.includes("FR")|| Message.includes("FR") && Message.includes("AI")) {
 		document.getElementById('result').innerHTML = `The message "${Message}" is legitimate!`;		
 	}
-	else if (Message.includes("AI") || Message.includes("aI") || Message.includes("ai")) {
+	else if (Message.includes("AI") || Message.includes("aI") || Message.includes("Ai")) {
 		document.getElementById('result').innerHTML = `The message "${Message}" is tampered with!`;
 	}
 	else {
